4355e13824b9a1d89082009a9989eee3cf1fbcb5
izzy2lost <izzynochill@gmail.com>
2026-07-30 13:13:29 -0400

    update build stuff

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    // AGP 9 has built-in Kotlin support, so org.jetbrains.kotlin.android is no longer
+    // declared here - applying it now fails outright.
+    id("com.android.application") version "9.2.1" apply false
 }
